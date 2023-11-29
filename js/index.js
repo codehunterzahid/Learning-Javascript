b5227dcd39d9716b375e59a15cfbbfaef514cd99
codehunterzahid <codehuntingwithzahid@gmail.com>
2023-11-29 20:14:31 +0500

    2 programs run

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// let number = prompt("Enter a number");
+
+// if (number % 7 === 0) {
+//     console.log(number, "is multiple of 7");
+// } else {
+//     console.log(number, "is not a multiple of 7");
+// }
+
+let marks = prompt("Enter your marks");
+
+if(marks >= 80 && marks <= 100){
+    console.log("Grade A");
+} else if(marks >= 70 && marks <= 79){
+    console.log("Grade B");
+} else if(marks >= 60 && marks <= 69){
+    console.log("Grade C");
+} else if(marks >= 50 && marks <= 59){
+    console.log("Grade D");
+} else {
+    console.log("Fail");
+}

d8c5fa3f48e4220ec9bf4269fde2abd5b708a6b8
codehunterzahid <codehuntingwithzahid@gmail.com>
2024-01-28 20:21:34 +0500

    new event listners explores

--- js/events.js
+++ js/events.js
@@ -3,20 +3,30 @@ const input = document.querySelector(".input");
 const txt = document.querySelector(".showText");
 
 btn1.addEventListener("dblclick", (evt) => {
-    input.style.display = "block";
-})
+  input.style.display = "block";
+});
 
-input.addEventListener("input",(event)=>{
-    txt.innerText = input.value;
-    let count = input.value.length;
-    if (count > 18){
-        alert("Max 18 characters allowed");
-        input.disabled = true;
-        txt.style.backgroundColor = "red";
-        input.style.cursor = "not-allowed";
-    } else{
-        txt.style.backgroundColor = "green";
+input.addEventListener("input", (event) => {
+  txt.innerText = input.value;
+  let count = input.value.length;
+  if (count >= 18) {
+    alert("Max 18 characters allowed");
+    input.disabled = true;
+    txt.style.color = "red";
+    input.style.cursor = "not-allowed";
+  } else {
+    txt.style.color = "green";
 
-        input.disabled = false;
-    }
-})
+    input.disabled = false;
+  }
+});
+
+let originalColor;
+txt.addEventListener("mouseenter", () => {
+  originalColor = txt.style.color;
+  txt.style.color = "Blue";
+});
+
+txt.addEventListener("mouseleave", () => {
+  txt.style.color = originalColor;
+});
